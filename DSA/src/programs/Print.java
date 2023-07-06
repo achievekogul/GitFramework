4fbf975a9e3ec38fc891864d4a26ca948801fff1
USER <USER@DESKTOP-V45LT2A>
2023-07-06 12:14:33 +0530

    Print

---
+++ DSA/src/programs/Print.java
@@ -0,0 +1,10 @@
+package programs;
+
+public class Print {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Going to start DSA Prgrams, Checking git commit and push");
+	}
+
+}
