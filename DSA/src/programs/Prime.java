2ae3c6bfa671e0728b6762668eff6953ca0dd9e5
achievekogul231 <achievekogul231@gmail.com>
2023-07-10 12:59:46 +0530

    Prime or not

---
+++ DSA/src/programs/Prime.java
@@ -0,0 +1,42 @@
+package programs;
+
+import java.util.Scanner;
+
+public class Prime {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		try (Scanner scn = new Scanner(System.in)) {
+			int t = scn.nextInt();
+			
+			
+			for(int i=0; i<t; i++) {
+				int n =scn.nextInt();
+				
+				int count=0;
+				
+				for(int div=2; div*div<=n; div++) {
+					
+					if(n%div==0) {
+						count++;
+						break;
+					}
+				}
+				
+				if(count==0) {
+					
+					System.out.println("Prime");
+				}
+				else {
+					System.out.println("Not Prime");
+				}
+				
+			}
+		}
+		
+		
+	}
+
+}
