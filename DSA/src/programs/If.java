3cdea8b9067c769a75319d000864d8c394f6b331
achievekogul231 <achievekogul231@gmail.com>
2023-07-10 11:54:22 +0530

    If

---
+++ DSA/src/programs/If.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class If {
+
+	public static void main(String[] args) {
+
+		int x=8;
+
+		if(x%2==0) {
+			System.out.println("Even number");
+
+		}
+		else {
+			System.out.println("odd number ");
+		}
+	}
+}
